012ed30e4f155912e275cc269b83fd1106170ae2
peterdesantis17 <peterdesantis2003@gmail.com>
2025-02-02 15:59:16 -0500

    Initial Commit

---
+++ 66 App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  66 App
+//
+//  Created by Peter on 2025-02-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 66 App/_6_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_AppApp.swift
+//  66 App
+//
+//  Created by Peter on 2025-02-02.
+//
+
+import SwiftUI
+
+@main
+struct _6_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
